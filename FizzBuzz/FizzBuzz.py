c2b3ae4907e550bb13b7999cfadd89902902e0f8
ArturMarekNowak <arturmareknowal@gmail.com>
2021-01-01 22:25:30 +0100

    added Fizz Buzz

---
+++ FizzBuzz/FizzBuzz.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+def fizzBuzz(N):
+
+    for i in range(1, N + 1):
+        
+        if(i % 5 == 0 and i % 3 == 0):
+            print("FizzBuzz")
+        
+        elif(i % 5 == 0):
+            print("Buzz")
+        
+        elif(i % 3 == 0):
+            print("Fizz")
+  
+        else:
+            print(i)
+
+fizzBuzz(100)
+
