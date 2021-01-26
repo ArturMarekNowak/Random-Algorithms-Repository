020206abfd750cd1ec1dbd762c88a180b25839a7
ArturMarekNowak <arturmareknowal@gmail.com>
2021-01-26 15:43:20 +0100

    added Binomial Coefficient in Python

---
+++ BinomialCoefficient/Python/BinomialCoefficient.py
@@ -0,0 +1,32 @@
+def factorial(N):
+
+    if (N == 1 or N == 0):
+        
+        return 1
+    
+    elif (N > 1):
+    
+        result = 1
+
+        for i in range(1, N + 1):
+            result *= i
+        
+        return result
+
+    elif (N < 0):
+        return "Error"
+
+
+
+def binomialCoefficient(listOfCoefficients):
+    
+    n = listOfCoefficients[0] 
+    k = listOfCoefficients[1] 
+
+    result = factorial(n) / (factorial(k) * factorial(n - k))
+
+    return result
+    
+
+listOne = [7, 2]
+print(binomialCoefficient(listOne))
