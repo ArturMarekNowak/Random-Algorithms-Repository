69b488ec03527d59c9c24e7f8a1f56e4f0e8acb9
ArturMarekNowak <arturmareknowal@gmail.com>
2021-02-07 12:25:25 +0100

    fix of Is Prime in Python

--- IsPrime/Python/IsPrime.py
+++ IsPrime/Python/IsPrime.py
@@ -1,10 +1,15 @@
 def isPrime(N):
+
+    if N == 1:
+        return False
+
     
     for i in range(2, N):
-        
+    
         if N % i == 0:
             return False
     
+    
     return True
 
-print(isPrime(101))
+print(isPrime(2))
