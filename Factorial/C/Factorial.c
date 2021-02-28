f1aa27f4b9b4b5a37d67b3248787df34423d2033
ArturMarekNowak <arturmareknowal@gmail.com>
2021-02-28 13:19:44 +0100

    added estimation formula in Factorial in C

--- Factorial/C/Factorial.c
+++ Factorial/C/Factorial.c
@@ -20,18 +20,17 @@ long long int recursFactorial(int * n)
 long long int iteratFactorial(int * n)
 {
 	long long int result = 1;
-	while(*n > 0)
-	{
-		result *= *n;
-		*n -= 1;
-	}
+		
+	for(int i = 1; i < *n + 1; i++)
+		result *= i;
+		
 	return result;
 }
 
-//long long int estimaFactorial(int * n)
-//{
-//	return (long long int) *n;
-//}
+long double estimaFactorial(int * n)
+{
+	return (long double) pow((2 * *n + 1.0/3.0) * M_PI, 0.5) * pow(*n, *n) * exp((-1) * *n);
+}
 
 int main(int argc, char** argv)
 {
@@ -43,15 +42,15 @@ int main(int argc, char** argv)
 	
 	printf("%lld\n", recursFactorial(&a));
 	printf("%lld\n", iteratFactorial(&a));
-	//printf("%lld\n", estimaFactorial(&a));
+	printf("%Lf\n", estimaFactorial(&a));
 	puts("");
 	printf("%lld\n", recursFactorial(&b));
 	printf("%lld\n", iteratFactorial(&b));
-	//printf("%lld\n", estimaFactorial(&b));
+	printf("%Lf\n", estimaFactorial(&b));
 	puts("");
 	printf("%lld\n", recursFactorial(&c));
 	printf("%lld\n", iteratFactorial(&c));
-	//printf("%lld\n", estimaFactorial(&c));
+	printf("%Lf\n", estimaFactorial(&c));
 	puts("");
 	
 	return 0;
