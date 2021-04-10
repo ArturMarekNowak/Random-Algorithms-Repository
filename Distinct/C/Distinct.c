3a036a8ab6d688f398746dc177b08bb0272c9efd
ArturMarekNowak <arturmareknowal@gmail.com>
2021-04-10 21:54:14 +0200

    added Distinct in C

---
+++ Distinct/C/Distinct.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int myCompare(const void * a, const void * b) {
+
+        int tempOne = * ((int * ) a);
+        int tempTwo = * ((int * ) b);
+        
+        if(tempOne == tempTwo) return 0;
+        else if(tempOne > tempTwo) return 1;
+        else return -1;
+    }
+
+int Distinct(int someArray[], int N) {
+    // write your code in C99 (gcc 6.2.0)
+
+    if(N == 0)
+        return 0;
+
+
+    qsort(someArray, N, sizeof(int), myCompare);
+
+    int previous = 0, result = 1;
+    for(int i = 1; i < N; i++)
+    {
+        previous = someArray[i - 1];
+        if(previous != someArray[i])
+            result += 1;
+    }
+    return result;
+}
+
+int main()
+{
+	int arrOne[] = {2, 1, 1, 2, 3, 1};
+
+	printf("%d\n", Distinct(arrOne, sizeof(arrOne)/sizeof(arrOne[0])));
+
+	return 0;
+}
