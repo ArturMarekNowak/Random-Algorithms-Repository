0b4b054d9704e61791536de99d6bf8a2a187cccf
ArturMarekNowak <arturmareknowal@gmail.com>
2021-04-21 14:08:07 +0200

    added Passing Cars in C

---
+++ PassingCars/C/PassingCars.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int PassingCars(int someArray[], int N)
+{
+	if(N == 1)
+        return 0;
+
+   	int counter = 0, result = 0;
+    	for(int i = 0; i < N; i++)
+    	{
+        	if(someArray[i] == 0)
+            		++counter;
+
+        	if(someArray[i] == 1)
+        	{
+            		result += counter;
+            		if(result > 1e9)
+                		return -1;
+        	}
+    	}
+
+    	return result;
+}
+
+int main()
+{
+	int arrOne[] = {0, 1, 0, 1, 1};
+
+	printf("%d\n", PassingCars(arrOne, sizeof(arrOne)/sizeof(arrOne[0])));
+
+	return 0;
+}
